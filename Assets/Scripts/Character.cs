4b51a88405cbe9fa79dc68f127317b17bf8feb7e
BarkhatovAV <barkhatov.a.v@mail.ru>
2023-08-13 14:44:28 +0300

    Add damage

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,5 +6,6 @@ public abstract class Character : MonoBehaviour
 {
 
     [field: SerializeField] public float Speed { get; protected set; } = 2f;
+    [field: SerializeField] public int MaxHealth { get; protected set; } = 10;
     public Vector3 Velocity { get; protected set; }
 }
